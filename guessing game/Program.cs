c4967ef95c0ceecd2a54db0e34cfc78e4ae40ec0
Johnson Osita <80931376+Germaine-jay@users.noreply.github.com>
2022-11-16 16:58:32 +0100

    init
    A guessing game

---
+++ guessing game/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guessing_game
+{
+    internal class Program
+    {
+        public int randomNumber;
+
+        public int numberOfGuesses;
+
+        public List<int> listOfGuesses = new List<int>();
+
+        Random secretNumber = new Random();
+
+        public int guessingGame()
+        {
+            randomNumber = secretNumber.Next(1, 10);
+            listOfGuesses.Add(0);
+
+            while (numberOfGuesses <= 3)
+            {
+                Console.Write("Enter your guess: ");
+                var playerGuess = int.Parse(Console.ReadLine());
+
+                int value;
+                if (int.TryParse(playerGuess.ToString(), out value) == false || !string.IsNullOrEmpty(playerGuess.ToString()))
+                {
+                    if (listOfGuesses.Last() == playerGuess)
+                    {
+                        Console.WriteLine("You entered the same wrong number");
+                        numberOfGuesses += 0;
+                    }
+                    if (listOfGuesses.Last() != playerGuess)
+                    {
+                        numberOfGuesses += 1;
+                        if (playerGuess > randomNumber && numberOfGuesses < 3)
+                        {
+                            listOfGuesses.Add(playerGuess);
+                            Console.WriteLine($"Lower, you have {4 - numberOfGuesses} attempts left");
+                        }
+
+                        if (playerGuess < randomNumber && numberOfGuesses < 3)
+                        {
+                            listOfGuesses.Add(playerGuess);
+                            Console.WriteLine($"Higher, you have {4 - numberOfGuesses} attempts left");
+                        }
+
+                        if (playerGuess > randomNumber && numberOfGuesses == 3)
+                        {
+                            listOfGuesses.Add(playerGuess);
+                            Console.WriteLine("Lower, your last attempt");
+                        }
+
+                        if (playerGuess < randomNumber && numberOfGuesses == 3)
+                        {
+                            listOfGuesses.Add(playerGuess);
+                            Console.WriteLine("Higher, your last attempt");
+                        }
+
+                        if (playerGuess != randomNumber && numberOfGuesses == 4)
+                        {
+                            Console.WriteLine("Game over, you have no attempts left");
+                        }
+                        else if (playerGuess == randomNumber)
+                        {
+                            Console.WriteLine("you are correct, big ups");
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("invalid input");
+                }
+            }
+            return numberOfGuesses;
+        }
+
+        static void Main(string[] args)
+        {
+            Program New = new Program();
+            New.guessingGame();
+        }
+    }
+}
