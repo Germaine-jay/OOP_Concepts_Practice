eafe8403b1e94b11e89aea8e45448c85002cfc00
Johnson Osita <80931376+Germaine-jay@users.noreply.github.com>
2022-11-16 17:00:00 +0100

    init

---
+++ leap year/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leap_year
+{
+    internal class Program
+    {
+        public int count;
+        public List<int> yearList = new List<int>();
+        public void leapYear()
+        {
+            while (yearList.Count != 20)
+            {
+                int year = 2022 + count;
+                int days = DateTime.DaysInMonth(year, 2);
+
+                if (days == 29)
+                {
+                    yearList.Add(year);
+                }
+                count++;
+            }
+            foreach (int year in yearList)
+            {
+                Console.WriteLine($"The year {year} is a leap year");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program New = new Program();
+            New.leapYear();
+        }
+    }
+}
