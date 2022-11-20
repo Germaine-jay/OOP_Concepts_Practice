afc2df586d0b582de858a91843401da07e3586c3
Johnson Osita <80931376+Germaine-jay@users.noreply.github.com>
2022-11-20 19:47:42 +0100

    Init

--- leap year/Program.cs
+++ leap year/Program.cs
@@ -8,18 +8,18 @@ namespace leap_year
 {
     internal class Program
     {
-        public int count;
-        public List<int> yearList = new List<int>();
+        private int count;
+        private List<int> yearList = new List<int>();
         public void leapYear()
         {
             while (yearList.Count != 20)
             {
-                int year = 2022 + count;
-                int days = DateTime.DaysInMonth(year, 2);
+                int Year = 2022 + count;
+                int Days = DateTime.DaysInMonth(Year, 2);
 
-                if (days == 29)
+                if (Days == 29)
                 {
-                    yearList.Add(year);
+                    yearList.Add(Year);
                 }
                 count++;
             }
